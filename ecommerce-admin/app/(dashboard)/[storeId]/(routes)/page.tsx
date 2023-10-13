aee0195fa6d111aaf055c20ca5771f30f85c23c9
Carlos Branty <losbrantybc@yahoo.com>
2023-10-13 00:10:24 -0500

    Inicial de todo

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () =>{
+    return (
+        <div>
+            este es nuestro Dashboard.
+        </div>
+    );
+}
+
+export default DashboardPage;
---
+++ ecommerce-admin/app/layout.tsx
@@ -0,0 +1,33 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+import { ModalProvider } from '@/providers/modal-provider'
+import { ToasterProvider } from '@/providers/toast-provider'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Admin Dashboard',
+  description: 'PunchArt POS Admin Dashboard',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+
+  return (
+    <ClerkProvider>
+      <html lang="es">
+        <body className={inter.className}>
+          <ToasterProvider/>
+          <ModalProvider/>
+          {children}
+          </body>
+      </html>
+    </ClerkProvider>
+    
+  )
+}
